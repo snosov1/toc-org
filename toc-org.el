3421f6192dee0b26d63e76851ce76a2df4f39abf
Sergei Nosov <sergei.nosov@gmail.com>
2015-04-21 12:46:50 +0300

    add a rename note to the commentary

--- toc-org.el
+++ toc-org.el
@@ -27,6 +27,9 @@
 ;; toc-org helps you to have an up-to-date table of contents in org files
 ;; without exporting (useful primarily for readme files on GitHub).
 
+;; NOTE: Previous name of the package is org-toc. It was changed because of a
+;; name conflict with one of the org contrib modules.
+
 ;; After installation put into your .emacs file something like
 
 ;; (if (require 'toc-org nil t)
