663e5ec2810e8eaeca7a58840e76c5a233531d49
Shohei YOSHIDA <syohex@gmail.com>
2021-03-23 19:22:30 +0900

    Correct sample configuration to valid code

--- toc-org.el
+++ toc-org.el
@@ -33,11 +33,12 @@
 ;; After installation put into your .emacs file something like
 
 ;; (if (require 'toc-org nil t)
-;;     (add-hook 'org-mode-hook 'toc-org-mode)
-
-;;     ;; enable in markdown, too
-;;     (add-hook 'markdown-mode-hook 'toc-org-mode)
-;;     (define-key markdown-mode-map (kbd "\C-c\C-o") 'toc-org-markdown-follow-thing-at-point)
+;;     (progn
+;;       (add-hook 'org-mode-hook 'toc-org-mode)
+;;
+;;       ;; enable in markdown, too
+;;       (add-hook 'markdown-mode-hook 'toc-org-mode)
+;;       (define-key markdown-mode-map (kbd "\C-c\C-o") 'toc-org-markdown-follow-thing-at-point))
 ;;   (warn "toc-org not found"))
 
 ;; And every time you'll be saving an org file, the first headline with a :TOC:
